8eaea2affd0503bf1df895e578e56b8f40b351e4
Kaan Kara <kaan.kara@inf.ethz.ch>
2019-05-06 18:41:09 +0200

    HW side changes to sgd_standalone

--- apps/glm/sw/tests/sgd_standalone.cpp
+++ apps/glm/sw/tests/sgd_standalone.cpp
@@ -6,7 +6,7 @@
 
 #define VALUE_TO_INT_SCALER 10
 
-// #define FPGA
+#define FPGA
 
 void thread_SGD(
 	uint32_t threadId,
@@ -43,7 +43,7 @@ int main(int argc, char* argv[]) {
 	numClasses = atoi(argv[6]);
 	sw0hw1 = atoi(argv[7]);
 
-	uint32_t partitionSize = 16000;
+	uint32_t partitionSize = 10400;
 
 #ifdef FPGA
 	Server server(false, true);
@@ -97,15 +97,19 @@ int main(int argc, char* argv[]) {
 	MemoryFormat format = RowStore;
 
 	if (sw0hw1 == 0) {
-		vector<float*> xHistories;
-		xHistories.resize(numClasses);
 		if (numClasses < 2) {
 			columnML[0]->AVXrowwise_SGD(type, nullptr, numEpochs, minibatchSize, stepSize, lambda, &args);
 		}
 		else {
-			vector<thread*> threads;
+			vector<float*> xHistories;
+			xHistories.resize(numClasses);
 			for (uint32_t c = 0; c < numClasses; c++) {
 				xHistories[c] = (float*)malloc(numEpochs*columnML[0]->m_cstore->m_numFeatures*sizeof(float));
+			}
+			vector<thread*> threads;
+
+			double start = get_time();
+			for (uint32_t c = 0; c < numClasses; c++) {
 				args.m_useOnehotLabels = true;
 				args.m_class = c;
 				threads.push_back(new thread(thread_SGD, c, columnML[0], type, xHistories[c], numEpochs, minibatchSize, stepSize, lambda, args));
@@ -114,51 +118,91 @@ int main(int argc, char* argv[]) {
 				threads[c]->join();
 				delete threads[c];
 			}
-			for (uint32_t c = 0; c < numClasses; c++) {
-				cout << "Class " << c << endl;
-				float* xHistory = xHistories[c];
-				args.m_useOnehotLabels = true;
-				args.m_class = c;
-				for (uint32_t e = 0; e < numEpochs; e++) {
-					cout << columnML[0]->Loss(type, xHistory + e*columnML[0]->m_cstore->m_numFeatures, lambda, &args) << endl;;
+			double total = get_time() - start;
+			cout << "Avg time per epoch: " << total/numEpochs << endl;
+			cout << "Processing rate: " << (numClasses*numEpochs*columnML[0]->GetDataSize())/total/1e9 << "GB/s" << endl;
+
+			for (uint32_t e = 0; e < numEpochs; e++) {
+				float loss = 0.0;
+				for (uint32_t c = 0; c < numClasses; c++) {
+					float* xHistory = xHistories[c];
+					args.m_useOnehotLabels = true;
+					args.m_class = c;
+					loss += columnML[0]->Loss(type, xHistory + e*columnML[0]->m_cstore->m_numFeatures, lambda, &args);
 				}
+				cout << loss/numClasses << endl;
+			}
+			for (uint32_t c = 0; c < numClasses; c++) {
 				free(xHistories[c]);
 			}
 		}
 	}
 	else {
 #ifdef FPGA
-		columnML[0]->CreateMemoryLayout(format, partitionSize, true);
-		for (uint32_t c = 1; c < numClasses; c++) {
-			columnML[c]->UseCreatedMemoryLayout(columnML[0]);
-		}
-
-		for (uint32_t c = 0; c < numClasses; c++) {
+		if (numClasses < 2) {
+			columnML[0]->CreateMemoryLayout(format, partitionSize, false);
 			if (minibatchSize == 1) {
-				columnML[c]->fSGD(type, numEpochs, stepSize, lambda, c);
+				columnML[0]->fSGD(type, numEpochs, stepSize, lambda, 0);
 			}
 			else {
-				columnML[c]->fSGD_minibatch(type, numEpochs, minibatchSize, stepSize, lambda, c);
+				columnML[0]->fSGD_minibatch(type, numEpochs, minibatchSize, stepSize, lambda, 0);
 			}
-		}
 
-		vector<FThread*> fthreads(numClasses);
-		for (uint32_t c = 0; c < numClasses; c++) {
-			fthreads[c] = server.Request(columnML[c]);
-		}
-		for (uint32_t c = 0; c < numClasses; c++) {
-			fthreads[c]->WaitUntilFinished();
-		}
+			double start = get_time();
+			FThread* fthread = server.Request(columnML[0]);
+			fthread->WaitUntilFinished();
+			double total = get_time() - start;
+			cout << "Avg time per epoch: " << total/numEpochs << endl;
+			cout << "Processing rate: " << (numEpochs*columnML[0]->GetDataSize())/total/1e9 << "GB/s" << endl;
 
-		// Verify
-		for (uint32_t c = 0; c < numClasses; c++) {
-			auto output = iFPGA::CastToFloat(columnML[c]->m_outputHandle);
+			// Verify
+			auto output = iFPGA::CastToFloat(columnML[0]->m_outputHandle);
 			float* xHistory = (float*)(output + 16);
-			args.m_useOnehotLabels = true;
-			args.m_class = c;
+			args.m_useOnehotLabels = false;
+			args.m_class = 0;
 			for (uint32_t e = 0; e < numEpochs; e++) {
 				float loss = columnML[0]->Loss(type, xHistory + e*columnML[0]->m_alignedNumFeatures, lambda, &args);
-				cout << "loss " << e << ": " << loss << endl;
+				cout << loss << endl;
+			}
+		}
+		else {
+			columnML[0]->CreateMemoryLayout(format, partitionSize, true);
+			for (uint32_t c = 1; c < numClasses; c++) {
+				columnML[c]->UseCreatedMemoryLayout(columnML[0]);
+			}
+
+			for (uint32_t c = 0; c < numClasses; c++) {
+				if (minibatchSize == 1) {
+					columnML[c]->fSGD(type, numEpochs, stepSize, lambda, c);
+				}
+				else {
+					columnML[c]->fSGD_minibatch(type, numEpochs, minibatchSize, stepSize, lambda, c);
+				}
+			}
+
+			double start = get_time();
+			vector<FThread*> fthreads(numClasses);
+			for (uint32_t c = 0; c < numClasses; c++) {
+				fthreads[c] = server.Request(columnML[c]);
+			}
+			for (uint32_t c = 0; c < numClasses; c++) {
+				fthreads[c]->WaitUntilFinished();
+			}
+			double total = get_time() - start;
+			cout << "Avg time per epoch: " << total/numEpochs << endl;
+			cout << "Processing rate: " << (numClasses*numEpochs*columnML[0]->GetDataSize())/total/1e9 << "GB/s" << endl;
+
+			// Verify
+			for (uint32_t e = 0; e < numEpochs; e++) {
+				float loss = 0.0;
+				for (uint32_t c = 0; c < numClasses; c++) {
+					auto output = iFPGA::CastToFloat(columnML[c]->m_outputHandle);
+					float* xHistory = (float*)(output + 16);
+					args.m_useOnehotLabels = true;
+					args.m_class = c;
+					loss += columnML[0]->Loss(type, xHistory + e*columnML[0]->m_alignedNumFeatures, lambda, &args);
+				}
+				cout << loss/numClasses << endl;
 			}
 		}
 #endif
