e298d47110729ea1ea794e2a7fd26814a997e940
Kaan Kara <kkara@r630-03.ethz.ch>
2019-05-26 13:53:20 +0200

    SLR assignment using TCL file

---
+++ apps/glm/xilinx_work/userPostSysLink.tcl
@@ -0,0 +1,4 @@
+set_property CONFIG.SLR_ASSIGNMENTS SLR0 [get_bd_cells xilinx_top_1]
+set_property CONFIG.SLR_ASSIGNMENTS SLR1 [get_bd_cells xilinx_top_i_1]
+set_property CONFIG.SLR_ASSIGNMENTS SLR2 [get_bd_cells xilinx_top_ii_1]
+set_property CONFIG.SLR_ASSIGNMENTS SLR2 [get_bd_cells xilinx_top_iii_1]
